86b0f55358e334a6a12f009950c292403932c8b8
Jianwei Xie <xiejw0217@gmail.com>
2018-09-08 20:46:49 -0700

    Add kernel config.

--- conf/gentoolet/sync.sh
+++ conf/gentoolet/sync.sh
@@ -2,5 +2,8 @@
 
 TARGET="/home/xiejw/Workspace/dotfiles/conf/$HOSTNAME"
 
+echo "Sync kernel config."
+cp -f /usr/src/linux/.config $TARGET/kernel.config
+
 echo "Sync /etc/portage/make.conf."
 cp -f /etc/portage/make.conf $TARGET/make.conf
