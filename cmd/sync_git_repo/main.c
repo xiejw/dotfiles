94bb420f6afe21f3c127f61cc98a71f8f5d17227
Jianwei Xie <xiejw0217@gmail.com>
2020-04-14 15:36:01 -0700

    Moved color_print to separated file.

--- cmd/sync_git_repo/main.c
+++ cmd/sync_git_repo/main.c
@@ -3,31 +3,10 @@
 
 #include "c/git.h"
 #include "c/path.h"
+#include "c/color_print.h"
 
 #define NO_SEVERE_ERROR 0
 #define MAX_PATH_LEN 100
-
-enum Color {
-  COLOR_INFO = 36,    /* Cyan */
-  COLOR_FYI = 33,     /* Yellow */
-  COLOR_ERROR = 31,   /* Red */
-  COLOR_SUCCESS = 32, /* Green */
-};
-
-/*
- * Prints the message with color. Flush is needed to ensure the color reset
- * applied in place immediately.
- *
- * Due to the limitation of variadic macro, if no argument is provided, use
- * `color_print`.
- */
-#define color_printf(c, fmt, ...)                     \
-  printf("\033[1;%dm" fmt "\033[0m", c, __VA_ARGS__); \
-  fflush(stdout)
-
-#define color_print(c, msg)              \
-  printf("\033[1;%dm" msg "\033[0m", c); \
-  fflush(stdout)
 
 int handle_repo(char* path) {
   DECLARE_ERROR(err);
