21d393ab18f6f263d7756ccb3999ee96060daad8
Jianwei Xie <xiejw0217@gmail.com>
2020-07-19 13:14:23 -0700

    refactor the code.

--- cmd/prompt/main.rs
+++ cmd/prompt/main.rs
@@ -1,21 +1,33 @@
 mod env {
-    use std::process::Command;
 
-    pub fn vim() -> bool {
-        let output = Command::new("ps")
-            .output()
-            .expect("failed to execute process");
+    pub mod vim {
+        use std::process::Command;
 
-        std::str::from_utf8(&output.stdout).unwrap().contains("vim")
+        /// Checks whether vim is running in background.
+        pub fn runnning_in_bg() -> bool {
+            let output = Command::new("ps")
+                .output()
+                .expect("failed to execute process");
+            std::str::from_utf8(&output.stdout).unwrap().contains("vim")
+        }
+    }
+
+    pub mod git {
+        use std::process::Command;
+
+        /// Checks whether current git repository has pending files..
+        pub fn has_pending_files() -> bool {
+            let output = Command::new("git")
+                .arg("status")
+                .arg("-s")
+                .output()
+                .expect("failed to execute process");
+            output.stdout.len() > 0
+        }
     }
 }
 
 fn main() {
-    // let output = Command::new("ls")
-    //     .arg("-l")
-    //     .arg("-h")
-    //     .output()
-    //     .expect("failed to execute process");
-    //
-    println!("contains vim: {}", env::vim());
+    println!("vim runnning_in_bg: {}", env::vim::runnning_in_bg());
+    println!("git has_pending_files: {}", env::git::has_pending_files());
 }
