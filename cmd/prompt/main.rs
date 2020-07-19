3b5b718aa0da93c104af26780df87697369245b4
Jianwei Xie <xiejw0217@gmail.com>
2020-07-18 17:41:09 -0700

    added comtains.

--- cmd/prompt/main.rs
+++ cmd/prompt/main.rs
@@ -1,11 +1,21 @@
-use std::process::Command;
+mod env {
+    use std::process::Command;
+
+    pub fn vim() -> bool {
+        let output = Command::new("ps")
+            .output()
+            .expect("failed to execute process");
+
+        std::str::from_utf8(&output.stdout).unwrap().contains("vim")
+    }
+}
 
 fn main() {
-    let output = Command::new("ls")
-        .arg("-l")
-        .arg("-h")
-        .output()
-        .expect("failed to execute process");
-
-    println!("{}", std::str::from_utf8(&output.stdout).unwrap())
+    // let output = Command::new("ls")
+    //     .arg("-l")
+    //     .arg("-h")
+    //     .output()
+    //     .expect("failed to execute process");
+    //
+    println!("contains vim: {}", env::vim());
 }
