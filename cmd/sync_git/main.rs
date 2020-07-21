3073db17956ff450a0559832f0a2fbb82074e859
Jianwei Xie <xiejw0217@gmail.com>
2020-07-21 16:20:19 -0700

    added new change

--- cmd/sync_git/main.rs
+++ cmd/sync_git/main.rs
@@ -1,3 +1,12 @@
+use std::process::Command;
 fn main() {
+    let mut command = Command::new("ls");
+    command.current_dir("~/Workspace");
+    if let Ok(mut child) = command.spawn() {
+        child.wait().expect("command wasn't running");
+        println!("Child has finished its execution!");
+    } else {
+        println!("ls command didn't start");
+    }
     println!("Hello");
 }
