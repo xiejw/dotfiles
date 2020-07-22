79b07328ec539b5288bc9cec0a139c41c7457937
Jianwei Xie <xiejw0217@gmail.com>
2020-07-21 19:03:12 -0700

    added expand.

--- cmd/sync_git/main.rs
+++ cmd/sync_git/main.rs
@@ -1,7 +1,10 @@
+extern crate shellexpand;
+
 use std::process::Command;
+
 fn main() {
     let mut command = Command::new("ls");
-    command.current_dir("~/Workspace");
+    command.current_dir(shellexpand::tilde("~/Workspace").as_ref());
     if let Ok(mut child) = command.spawn() {
         child.wait().expect("command wasn't running");
         println!("Child has finished its execution!");
