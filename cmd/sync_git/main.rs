6bc624c8379fac0bbd938f02512051875ed38888
Jianwei Xie <xiejw0217+xiejw2g@gmail.com>
2020-07-21 19:34:58 -0700

    split as a func.

--- cmd/sync_git/main.rs
+++ cmd/sync_git/main.rs
@@ -2,9 +2,13 @@ extern crate shellexpand;
 
 use std::process::Command;
 
+fn expand_path(path: &str) -> String {
+    shellexpand::tilde(path).into_owned()
+}
+
 fn main() {
     let mut command = Command::new("ls");
-    command.current_dir(shellexpand::tilde("~/Workspace").as_ref());
+    command.current_dir(expand_path("~/Workspace"));
     if let Ok(mut child) = command.spawn() {
         child.wait().expect("command wasn't running");
         println!("Child has finished its execution!");
