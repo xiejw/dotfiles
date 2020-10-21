fae719978131e9e7ce9d4068e34907c754352e8e
Jianwei Xie <xiejw0217@gmail.com>
2020-10-21 09:28:57 -0700

    refactored the path.h

--- c/path.h
+++ c/path.h
@@ -1,28 +1,19 @@
-#ifndef DOTFILES_C_PATH_H_
-#define DOTFILES_C_PATH_H_
+#ifndef PATH_H_
+#define PATH_H_
 
 #include <string.h>
 #include <wordexp.h>
 
 #include "c/error.h"
 
-/*
- * Expands the `~` in the path.
- *
- * Args:
- *   - `dst` must be large enough to hold the result.
- *
- * Returns:
- *   - OK, ENOTPATH, ETOOMANYRESULTS.
- */
+// Expands the `~` in the path. `dst` must be large enough to hold the result.
 error_t expand_tilde_path(char* original_path, char* dst) {
   wordexp_t exp_result;
   if (OK != wordexp(original_path, &exp_result, /*flags=*/0)) {
     return ENOTPATH;
   }
 
-  /* Only the simpliest case is supported: one output. So error out for others.
-   */
+  // Only the simpliest case is supported: one output. So error out for others.
   if (exp_result.we_wordc != 1) {
     wordfree(&exp_result);
     return ETOOMANYRESULTS;
