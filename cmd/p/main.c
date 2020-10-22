aa37e10ee5343c31b9498785d9bfc552d17ed21d
Jianwei Xie <xiejw0217@gmail.com>
2020-10-21 21:30:34 -0700

    refactor the execCmd as a lib.

--- cmd/p/main.c
+++ cmd/p/main.c
@@ -1,71 +1,50 @@
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /* Read characters from the pipe and echo them to stdout. */
 
-void
-read_from_pipe (int file)
-{
+void read_from_pipe(int file) {
   FILE *stream;
-  int c;
-  stream = fdopen (file, "r");
-  while ((c = fgetc (stream)) != EOF)
-    putchar (c);
-  fclose (stream);
+  int   c;
+  stream = fdopen(file, "r");
+  while ((c = fgetc(stream)) != EOF) {
+    putchar(c);
+  }
+  fclose(stream);
 }
 
-/* Write some random text to the pipe. */
-
-void
-write_to_pipe (int file)
-{
-  FILE *stream;
-  stream = fdopen (file, "w");
-  fprintf (stream, "hello, world!\n");
-  fprintf (stream, "goodbye, world!\n");
-  fclose (stream);
-}
-
-int
-main (void)
-{
+int execCmd(char **cmd) {
   pid_t pid;
-  int mypipe[2];
+  int   mypipe[2];
 
-  /* Create the pipe. */
-  if (pipe (mypipe))
-    {
-      fprintf (stderr, "Pipe failed.\n");
-      return EXIT_FAILURE;
-    }
+  if (pipe(mypipe)) {
+    fprintf(stderr, "Pipe failed.\n");
+    return -1;
+  }
 
   /* Create the child process. */
-  pid = fork ();
-  if (pid == (pid_t) 0)
-    {
-      /* This is the child process.
-         Close other end first. */
-      close (mypipe[0]);
-      dup2(mypipe[1], 1);
-      char* grep_args[] = {"-lh", NULL};
-      printf("==launch ls");
-      execvp("ls", grep_args);
-      return EXIT_SUCCESS;
-    }
-  else if (pid < (pid_t) 0)
-    {
-      /* The fork failed. */
-      fprintf (stderr, "Fork failed.\n");
-      return EXIT_FAILURE;
-    }
-  else
-    {
-      /* This is the parent process.
-         Close other end first. */
-      close (mypipe[1]);
-      read_from_pipe (mypipe[0]);
-      return EXIT_SUCCESS;
-    }
+  pid = fork();
+  if (pid == (pid_t)0) {
+    /* This is the child process.  Close the reading end first. */
+    close(mypipe[0]);
+    dup2(mypipe[1], 1);
+    execvp(cmd[0], cmd);
+    return 0;  // never reached.
+  } else if (pid < (pid_t)0) {
+    /* The fork failed. */
+    fprintf(stderr, "Fork failed.\n");
+    return -1;
+  } else {
+    /* This is the parent process.  Close writing end first. */
+    close(mypipe[1]);
+    return mypipe[0];
+  }
+}
+
+int main(void) {
+  char *args[] = {"ls", "-l", NULL};
+  int   fd     = execCmd(args);
+  read_from_pipe(fd);
 }
