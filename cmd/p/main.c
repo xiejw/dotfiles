499f8dedccad1cc40d83b1953cbf835cd90a1474
Jianwei Xie <xiejw0217@gmail.com>
2020-10-22 20:09:08 -0700

    added brach name.

--- cmd/p/main.c
+++ cmd/p/main.c
@@ -7,20 +7,31 @@
 // Prototype.
 // ----------------------------------------------------------------------------
 
+#define YES 1
+#define NO  0
+
 int vimRunInBg();
 int gitPending();
+int gitBranceName(char*);
 
 int main(void) {
-  if (vimRunInBg()) {
+  if (YES == vimRunInBg()) {
     cPrint(COLOR_ERROR, "v\n");
   } else {
     cPrint(COLOR_ERROR, "vn\n");
   }
 
-  if (gitPending()) {
+  if (YES == gitPending()) {
     cPrint(COLOR_ERROR, "g\n");
   } else {
     cPrint(COLOR_ERROR, "gn\n");
+  }
+
+  char name[100];
+  if (YES == gitBranceName(name)) {
+    cPrintf(COLOR_ERROR, "b: %s\n", name);
+  } else {
+    cPrint(COLOR_ERROR, "bn\n");
   }
 }
 
@@ -39,14 +50,14 @@ int vimRunInBg() {
     return EUNSPECIFIED;
   }
 
-  int  result = 0;
+  int  result = NO;
   char line[MAX_STR_LINE_LEN];
   while (1) {
     int len = frNextLine(handle, line);
     if (len == 0) break;
 
     if (strstr(line, "vim") != NULL) {
-      result = 1;
+      result = YES;
       break;
     }
   }
@@ -65,12 +76,38 @@ int gitPending() {
     return EUNSPECIFIED;
   }
 
-  int  result = 0;
+  int  result = NO;
   char line[MAX_STR_LINE_LEN];
   // read oneline is enough.
   int len = frNextLine(handle, line);
   if (len > 0) {
-    result = 1;
+    result = YES;
+  }
+  frClose(handle);
+  return result;
+}
+
+int gitBranceName(char* name) {
+  char* args[] = {"git", "branch", "--no-color", NULL};
+  int   fd     = execCmd(args);
+
+  fr_handle_t* handle;
+  if (frDOpen(&handle, fd) != OK) {
+    cPrint(COLOR_ERROR, "Error: failed to call ps");
+    close(fd);
+    return EUNSPECIFIED;
+  }
+
+  int  result = NO;
+  char line[MAX_STR_LINE_LEN];
+  while (1) {
+    int len = frNextLine(handle, line);
+    if (len == 0) break;
+    if (len < 3) continue;
+    if (line[0] != '*') continue;
+    strcpy(name, line + 2);
+    result = YES;
+    break;
   }
   frClose(handle);
   return result;
